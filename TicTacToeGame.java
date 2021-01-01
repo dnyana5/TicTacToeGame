1e877cb787b499c73c633df5e50b0804046850b0
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-01 16:54:09 +0530

    Added:Determining user letter and taking inputs from user

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,7 @@
 package TicTacToe;
 public class TicTacToeGame {
 	char[] board;
+	static char playerCharactor,cpuCharactor;
 public void assignBoard() 
 	{
 		board = new char[10];
@@ -8,9 +9,31 @@ public void assignBoard()
 			board[i] = ' ';
 		}
 	}
+	public void userInput() {
+			System.out.println("select x or o");
+			Scanner input = new Scanner(System.in);
+			char userInput = input.next().toUpperCase().charAt(0);
+			System.out.println(userInput);
+			
+			if ( 'X' == userInput) {
+				playerCharactor = 'X';
+				cpuCharactor = 'O';
+			}
+			else if ('O' == userInput ) {
+				cpuCharactor = 'X';
+				playerCharactor = 'O';
+			}
+			else {
+				System.out.println("enter a valid choice");
+				userInput();
+			}
+		}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
 		TicTacToeGame game = new TicTacToeGame();
 		game.assignBoard();
+		game.userInput();
+		System.out.println("player charater is -" +playerCharactor + 
+					" computer character is - "+cpuCharactor);
 	}
 }
