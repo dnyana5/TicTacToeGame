5d77be2f5f68ff7bef68d6faca9a991e21a270f1
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-01 16:06:55 +0530

    Initialising game Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,16 @@
 package TicTacToe;
 public class TicTacToeGame {
+	char[] board;
+public void assignBoard() 
+	{
+		board = new char[10];
+		for (int i = 1 ; i < 10 ; i++ ) {
+			board[i] = ' ';
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
+		TicTacToeGame game = new TicTacToeGame();
+		game.assignBoard();
 	}
 }
